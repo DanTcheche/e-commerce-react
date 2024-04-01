d9912424d6da0b3435e2810f426cd504dc13a74a
Dan Tcheche <dtcheche@gmail.com>
2024-04-01 12:05:59 -0300

    Add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
-import React, { StrictMode } from "react";
-import ReactDOM, { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Router } from "./router/index.ts";
 
 const queryClient = new QueryClient();
 
@@ -10,14 +11,16 @@ const root = document.getElementById("root");
 
 if (!root) {
   throw new Error(
-    "There's no #root div, something's wrong with our index.html",
+    "There's no #root div, something's wrong with our index.html"
   );
 }
 
 createRoot(root).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </QueryClientProvider>
-  </StrictMode>,
+  </StrictMode>
 );
