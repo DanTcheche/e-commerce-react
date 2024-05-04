82b96604475c2fd79abd07f611827e6ceb8837cd
Dan Tcheche <dtcheche@gmail.com>
2024-05-04 16:46:53 -0300

    Setup search bar

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,16 +2,35 @@ import { useQuery } from "@tanstack/react-query";
 import { ProductList } from "@/components/ProductList";
 import { getProducts } from "@/api/getProducts";
 import { Product } from "@/components/Product";
+import { SearchBar } from "../SearchBar";
+import { useState } from "react";
+import useDebounce from "@/hooks/useDebounce";
 
 export const Home = () => {
   const { data: products } = useQuery({
     queryKey: ["getProducts"],
     queryFn: getProducts,
   });
+  const [filter, setFilter] = useState<string | undefined>();
+
+  let searchedProducts = products?.filter((product) =>
+    product.title.toLowerCase().includes(filter || "")
+  );
+  const handleFilterChange = () => {
+    searchedProducts = products?.filter((product) =>
+      product.title.toLowerCase().includes(filter || "")
+    );
+  };
+  useDebounce({ value: filter, handleChange: handleFilterChange });
   return (
-    <ProductList>
-      {products &&
-        products.map((item, key) => <Product key={key} product={item} />)}
-    </ProductList>
+    <>
+      <SearchBar onValueChange={(value: string) => setFilter(value)} />
+      <ProductList>
+        {searchedProducts &&
+          searchedProducts.map((item, key) => (
+            <Product key={key} product={item} />
+          ))}
+      </ProductList>
+    </>
   );
 };
